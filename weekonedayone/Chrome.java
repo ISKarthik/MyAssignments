447e7e465f7e71b1bdadb6a4a81272bb0802dd1c
ISKarthik <118063393+ISKarthik@users.noreply.github.com>
2025-03-29 21:33:05 +0530

    Add files via upload

---
+++ weekonedayone/Chrome.java
@@ -0,0 +1,23 @@
+package weekonedayone;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		
+		String developer = "Google";
+		boolean isbeta = false;
+		int releaseyear = 2008;
+		char shortcutKey = 'C';
+			
+		System.out.println((int)releaseyear);
+		System.out.println((boolean) isbeta);
+		System.out.println((String)developer);
+		System.out.println((char)shortcutKey);
+		System.out.println((float)version);
+		
+
+	}
+
+}
